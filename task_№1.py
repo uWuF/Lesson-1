22685ebf37f0b7dba19e09e2c8e1c73a49259f88
Richard Yankevich <richardyankevich@gmail.com>
2022-07-14 14:39:10 +0300

    Task 1

---
+++ task_№1.py
@@ -0,0 +1,8 @@
+gender = "male"
+print(gender)
+city = "SPb"
+print(city)
+food = input("What kind of food do you like?: ")
+print(f'Awesome, I like {food} too!')
+number = int(input("How many meals do you have a day?: "))
+print(f"{number} - good boy")
