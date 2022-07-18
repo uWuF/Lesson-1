4803d93258ff1fc5891d45cbefc9cf7f6f474793
Richard Yankevich <richardyankevich@gmail.com>
2022-07-18 20:53:24 +0300

    Task 2

--- task_№3.py
+++ task_№3.py
@@ -1,3 +1,14 @@
-n = (input('Введите число: '))
+seasons = ['winter', 'spring', 'summer', 'fall']
 
-print(int(n) + int(n + n) + int(n + n + n))
+number = int(input('Введите число от 1 до 12: '))
+
+if number == 12 or number <= 2:
+    print(seasons[0])
+elif 2 < number < 6:
+    print(seasons[1])
+elif 5 < number < 9:
+    print(seasons[2])
+elif 8 < number < 12:
+    print(seasons[3])
+else:
+    print('Неверное число')
