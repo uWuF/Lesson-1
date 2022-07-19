c065a2c3877b11cbb39fb5147750d6b685dc8cec
Richard Yankevich <richardyankevich@gmail.com>
2022-07-19 12:43:59 +0300

    Task 3

--- task_№4.py
+++ task_№4.py
@@ -1,12 +1,7 @@
-n = int(input('Введите число: '))
+my_str = input('Введите строку: ')
 
-a = n % 10
+b = 1
 
-b = 0
-
-while n > 0:
-    b = (n // 10) % 10
-    n = n // 10
-    if b > a:
-        a = b
-print(a)
+for i in (my_str.split(' ')):
+    print(f"{b}.", i[:10])
+    b += 1
