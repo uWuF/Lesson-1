056dfb390bb37315adc3fd76e7e1f4f6daade00b
Richard Yankevich <richardyankevich@gmail.com>
2022-07-19 21:59:36 +0300

    Task 5

--- task_№5.py
+++ task_№5.py
@@ -1,14 +1,22 @@
-gain = float(input('Введите прибыль фирмы: '))
+number = int(input('Введите число: '))
 
-spent = float(input('Введите траты фирмы: '))
+my_list = [number]
 
-a = gain - spent
+print(my_list)
 
-b = 0
+n = 0
 
-if a < 0:
-    a = spent - gain
-    print(f'Убыток составил {a} рублей.')
-else:
-    b = int(input('Введите количество сотрудников: '))
-    print(f'Прибыль на одного сотрудника составила: {a / b} рублей.')
+while n != 999:
+    n = int(input('Введите число (для завершения программы введите 999): '))
+    if n in my_list:
+        my_list.insert((my_list.index(n) + my_list.count(n)), (n))
+    elif n > my_list[0]:
+        my_list.insert(0, n)
+    elif n < my_list[-1]:
+        my_list.append(n)
+    else:
+        for i in range(len(my_list)):
+            if my_list[i] > n and my_list[i + 1] < n:
+                my_list.insert(i + 1, n)
+
+    print(my_list)
