9219004a1184e00a2272e844ea723cf8abf6e357
Richard Yankevich <richardyankevich@gmail.com>
2022-07-19 12:25:10 +0300

    Task 3

--- task_№3.py
+++ task_№3.py
@@ -12,3 +12,10 @@ elif 8 < number < 12:
     print(seasons[3])
 else:
     print('Неверное число')
+
+seasons = {12: 'winter', 1: 'winter', 2: 'winter', 3: 'spring', 4: 'spring', 5: 'spring', 6: 'summer', 7: 'summer',
+           8: 'summer', 9: 'fall', 10: 'fall', 11: 'fall'}
+
+number = int(input('Введите число от 1 до 12: '))
+
+print(seasons.get(number))
