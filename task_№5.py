ede1adf1281bef36cc300da7e4b72e00cd38057d
Richard Yankevich <richardyankevich@gmail.com>
2022-08-13 13:00:02 +0300

    Task 5

--- task_№5.py
+++ task_№5.py
@@ -1,14 +1,19 @@
-gain = float(input('Введите прибыль фирмы: '))
+def my_func():
+    sum_of_num = 0
+    while True:
+        err = False
+        numbers = input('Введите целые числа через пробел, для выхода введите q: ').split()
+        for i in numbers:
+            if i == 'q':
+                return sum_of_num
+            else:
+                try:
+                    sum_of_num += int(i)
+                except ValueError:
+                    err = True
+        if err:
+            print('Вводите только целые числа!')
+        print(f'Сумма введённых чисел: {sum_of_num}')
 
-spent = float(input('Введите траты фирмы: '))
 
-a = gain - spent
-
-b = 0
-
-if a < 0:
-    a = spent - gain
-    print(f'Убыток составил {a} рублей.')
-else:
-    b = int(input('Введите количество сотрудников: '))
-    print(f'Прибыль на одного сотрудника составила: {a / b} рублей.')
+print(my_func())
