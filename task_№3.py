8aa9afe779391851f5dd8b361c3e1c75539aa474
Richard Yankevich <richardyankevich@gmail.com>
2022-08-13 12:03:26 +0300

    Task 3

--- task_№3.py
+++ task_№3.py
@@ -1,3 +1,9 @@
-n = (input('Введите число: '))
+def my_func(num_1, num_2, num_3):
+    my_list = [num_1, num_2, num_3]
+    my_list.remove(min(my_list))
+    return sum(my_list)
 
-print(int(n) + int(n + n) + int(n + n + n))
+
+print(my_func(float(input('Первое число: ')),
+              float(input('Второе число: ')),
+              float(input('Третье число: '))))
