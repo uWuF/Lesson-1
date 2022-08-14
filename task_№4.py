c6a28780ce1f63ac41657e28a5ec6d1325fb18f3
Richard Yankevich <richardyankevich@gmail.com>
2022-08-14 15:40:19 +0300

    task_4

--- task_№4.py
+++ task_№4.py
@@ -1,12 +1,17 @@
-n = int(input('Введите число: '))
+my_list = list(map(int, (input('Введите целые числа через пробел: ').split())))
 
-a = n % 10
+print(my_list)
 
-b = 0
+my_list_copy = []
 
-while n > 0:
-    b = (n // 10) % 10
-    n = n // 10
-    if b > a:
-        a = b
-print(a)
+for i in range(len(my_list)):
+    if my_list[i] not in my_list_copy:
+        my_list_copy.append(my_list[i])
+
+for i in range(len(my_list_copy)):
+    if my_list_copy[i] in my_list:
+        my_list.remove(my_list_copy[i])
+
+new_list = [my_list_copy[num] for num in range(len(my_list_copy)) if my_list_copy[num] not in my_list]
+
+print(new_list)
