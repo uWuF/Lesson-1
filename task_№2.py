4485f73ce2e251c34d52e9287415dae3c8770ecd
Richard Yankevich <richardyankevich@gmail.com>
2022-08-14 14:25:07 +0300

    task_2

--- task_№2.py
+++ task_№2.py
@@ -1,9 +1,5 @@
-number = int(input("Введите время в секундах: "))
+my_list = [0, 0, 0, 44, 5, 21, -2, -1]
 
-hour = number // 3600
+new_list = [my_list[i] for i in range(1, len(my_list)) if my_list[i] > my_list[i - 1]]
 
-minute = (number - ((number // 3600) * 3600)) // 60
-
-second = number - (((number // 3600) * 3600) + (minute * 60))
-
-print(f"{hour:02}:{minute:02}:{second:02}")
+print(new_list)
